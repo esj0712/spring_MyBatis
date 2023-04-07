a150683d200deff6fc2849e5edf9aac1da6aab03
82102 <82102@DESKTOP-81LUTCC>
2023-04-07 10:09:26 +0900

    230407

--- src/main/java/kr/co/softsoldesk/main/MainClass.java
+++ src/main/java/kr/co/softsoldesk/main/MainClass.java
@@ -1,20 +1,47 @@
 package kr.co.softsoldesk.main;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.List;
 
-import kr.co.softsoldesk.beans.TestBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import kr.co.softsoldesk.beans.JdbcBean;
+import kr.co.softsoldesk.config.BeanConfigClass;
+import kr.co.softsoldesk.mapper.MapperInterface;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("kr/co/softsoldesk/config/beans.xml");
+		System.out.println("-------------------------------JAVA----------------------------------");
+
+		AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(BeanConfigClass.class);
 		
-		TestBean bean1=ctx.getBean("xml1", TestBean.class);
-		int a1=bean1.method1();
+		MapperInterface mapper=ctx.getBean("test_mapper", MapperInterface.class);
 		
-		System.out.println("점수 : "+a1);
+		JdbcBean bean2=new JdbcBean();
+		//bean2.setInt_data(12);
+		//bean2.setStr_data("Project Setting");
+		//mapper.insert_data(bean2);
 		
+		// select
+		List<JdbcBean> list=mapper.select_data();
+		for(JdbcBean bean1:list){
+			System.out.println("int_data : "+bean1.getInt_data());
+			System.out.println("str_data : "+bean1.getStr_data());
+			System.out.println("---------------------------------");
+		}
+		// updata
+		//JdbcBean bean3=new JdbcBean();
+		//bean3.setStr_data("aaaaa");
+		//bean3.setInt_data(10);
+		//mapper.update_data(bean3);
+		
+		// delete
+		mapper.delete_data(10);
+		
+		
+		
+		System.out.println("저장완료");
 		ctx.close();
 	}
 
